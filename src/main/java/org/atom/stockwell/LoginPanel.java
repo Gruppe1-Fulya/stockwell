fa8c0b397a0417d274c4dbb7245e00eac32dccf7
Burak <69385981+burakarslan8@users.noreply.github.com>
2023-05-04 13:38:52 +0300

    dialogs added

--- src/main/java/org/atom/stockwell/LoginPanel.java
+++ src/main/java/org/atom/stockwell/LoginPanel.java
@@ -10,6 +10,8 @@ import javax.swing.*;
 import javax.xml.crypto.Data;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.List;
 
 public class LoginPanel extends JPanel{
@@ -21,6 +23,45 @@ public class LoginPanel extends JPanel{
 
     public LoginPanel(MainFrame mainFrame){
 
+        // Submitting information by pressing enter
+        passwordField.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if(e.getKeyCode()==KeyEvent.VK_ENTER){
+                    String username = usernameField.getText();
+                    String password = new String(passwordField.getPassword()); // passwordlar char[] olarak aliniyor
+
+
+                    DatabaseManager db = new DatabaseManager();
+                    List<Mitarbeiter> mitarbeiterList = db.getMitarbeiterList();
+
+                    if(mitarbeiterList
+                            .stream()
+                            .anyMatch(mitarbeiter ->
+                                    mitarbeiter.getUsername().equals(username) &&
+                                            mitarbeiter.getPassword().equals(password))
+                    ){
+                        System.out.println("show Home Screen");
+                        mainFrame.showHomePanel();
+                    }else{
+                        JOptionPane.showMessageDialog(mainFrame,
+                                "Sie haben einen ungültigen Benutzername oder Passwort eingegeben.",
+                                "Fehler",
+                                JOptionPane.ERROR_MESSAGE);
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+            }
+        });
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -39,6 +80,11 @@ public class LoginPanel extends JPanel{
                 ){
                     System.out.println("show Home Screen");
                     mainFrame.showHomePanel();
+                }else{
+                    JOptionPane.showMessageDialog(mainFrame,
+                            "Sie haben einen ungültigen Benutzername oder Passwort eingegeben.",
+                            "Fehler",
+                            JOptionPane.ERROR_MESSAGE);
                 }
             }
         });
