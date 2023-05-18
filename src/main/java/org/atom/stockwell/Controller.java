285532184683754b64f21e706c7cbebef029aad8
caglarkabaca <cacarekt@yandex.com>
2023-05-18 20:10:45 +0300

    controller ++

--- src/main/java/org/atom/stockwell/Controller.java
+++ src/main/java/org/atom/stockwell/Controller.java
@@ -8,8 +8,11 @@ package org.atom.stockwell;
 import org.atom.stockwell.db.DatabaseManager;
 import org.atom.stockwell.db.classes.Lager;
 import org.atom.stockwell.db.classes.LagerProduct;
+import org.atom.stockwell.db.classes.Product;
+import org.atom.stockwell.db.classes.Transaktion;
 
 import javax.swing.table.DefaultTableModel;
+import javax.xml.crypto.Data;
 import java.util.List;
 
 public class Controller {
@@ -64,6 +67,77 @@ public class Controller {
         }
 
         return tableModel;
+    }
+
+    // AddProductDB Template
+    public static boolean AddProductDB(Product product) throws Exception {
+        DatabaseManager db = new DatabaseManager();
+
+        // check if Product in db exits
+        if (db.getProductList()
+                .stream()
+                .anyMatch(p -> p.getId().equals(product.getId())))
+            throw new Exception("[SW] PRODUCT ALREADY EXITS");
+
+        return db.createNewProduct(product);
+    }
+
+    public static boolean AddProductLagerDB(LagerProduct product) throws Exception {
+
+        DatabaseManager db = new DatabaseManager();
+
+        Lager lager = db.getLager();
+
+        if (!lager.lagerProducts()
+                .stream()
+                .anyMatch(lp -> lp.getId().equals(product.getId()))) {
+            lager.addProduct(product);
+
+            if (!db.updateLager(lager))
+                throw new Exception("[SW] DB ERROR");
+
+            return db.getLager().lagerProducts()
+                    .stream()
+                    .anyMatch(lp -> lp.getId().equals(product.getId()));
+
+        }
+
+        throw new Exception("[SW] PRODUCT ALREADY EXITS");
+    }
+
+    public static boolean AddTransaktionDB(Transaktion transaktion) throws Exception {
+
+        DatabaseManager db = new DatabaseManager();
+
+        if (!db.getProductList()
+                .stream()
+                .anyMatch(p -> p.getId().equals(transaktion.getProduct().getId())))
+            throw new Exception("[SW] PRODUCT DOESN'T EXITS");
+
+        if (!db.getPersonList()
+                .stream()
+                .anyMatch(p -> p.getId().equals(transaktion.getKunde().getId())))
+            throw new Exception("[SW] KUNDE DOESN'T EXITS");
+
+        if (!db.getMitarbeiterList()
+                .stream()
+                .anyMatch(p -> p.getId().equals(transaktion.getMitarbeiter().getId())))
+            throw new Exception("[SW] MITARBEITER DOESN'T EXITS");
+
+        if (db.getTransaktions()
+                .stream()
+                .anyMatch(t -> t.getId().equals(transaktion.getId())))
+            throw new Exception("[SW] TRANSAKTION ALREADY EXITS");
+
+
+        if (!db.createNewTransaktion(transaktion))
+            throw new Exception("[SW] DB ERROR");
+
+        // returns check
+        return db.getTransaktions()
+                .stream()
+                .anyMatch(t -> t.getId().equals(transaktion.getId()));
+
     }
 
 }
